7d159c6185cfd0ea3bd375726ba9ac12bf2d73b5
Bruno Totoli <santosbruno101062@gmail.com>
2022-04-08 13:32:08 -0300

    Aula04

--- src/Aula03/program/Application.java
+++ src/Aula03/program/Application.java
@@ -4,6 +4,8 @@ import Aula03.entities.Product;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
 
 public class Application {
     public static void main(String[] args) {
@@ -12,7 +14,13 @@ public class Application {
         list.add(new Product("Mouse", 50.00));
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
-        list.forEach(product -> product.setPrice((product.getPrice() * 1.1)));
-        list.forEach(System.out::println);
+//        list.forEach(product -> product.setPrice((product.getPrice() * 1.1)));
+//        list.forEach(System.out::println);
+
+        //Aula 04
+        List<String> names = list.stream()
+                .map(product -> product.getName().toUpperCase())
+                .toList();
+        names.forEach(System.out::println);
     }
 }

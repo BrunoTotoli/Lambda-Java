af94497331f7608a9e5dc3c3f69beb491856a351
Bruno Totoli <santosbruno101062@gmail.com>
2022-04-08 18:35:09 -0300

    Aula06-Streams

---
+++ src/Aula06/program/Application.java
@@ -0,0 +1,24 @@
+package Aula06.program;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Application {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(3, 4, 5, 6, 7);
+
+        Stream<Integer> integerStream = list.stream().map(x -> x * 10);
+        System.out.println(Arrays.toString(integerStream.toArray()));
+
+        Stream<String> stringStream = Stream.of("Jairo", "Cleito", "Furduncio");
+        System.out.println(Arrays.toString(stringStream.toArray()));
+
+        Stream<Integer> iterateStream = Stream.iterate(0, x -> x + 2);
+        System.out.println(Arrays.toString(iterateStream.limit(10).toArray()));
+
+        Stream<Long> fibonacciSequenceStream = Stream.iterate(new Long[]{0L, 1L}, p -> new Long[]{p[1], p[0] + p[1]}).map(p -> p[0]);
+        System.out.println(Arrays.toString(fibonacciSequenceStream.limit(10).toArray()));
+
+    }
+}

da0c9bdbd390dda8affeca2537718672cd29e2d9
Bruno Totoli <santosbruno101062@gmail.com>
2022-04-08 22:49:00 -0300

    Aula07 Pipelines

--- src/Aula06/program/Application.java
+++ src/Aula06/program/Application.java
@@ -2,6 +2,7 @@ package Aula06.program;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Application {
@@ -9,16 +10,32 @@ public class Application {
         List<Integer> list = Arrays.asList(3, 4, 5, 6, 7);
 
         Stream<Integer> integerStream = list.stream().map(x -> x * 10);
-        System.out.println(Arrays.toString(integerStream.toArray()));
+//        System.out.println(Arrays.toString(integerStream.toArray()));
 
         Stream<String> stringStream = Stream.of("Jairo", "Cleito", "Furduncio");
-        System.out.println(Arrays.toString(stringStream.toArray()));
+//        System.out.println(Arrays.toString(stringStream.toArray()));
 
         Stream<Integer> iterateStream = Stream.iterate(0, x -> x + 2);
-        System.out.println(Arrays.toString(iterateStream.limit(10).toArray()));
+//        System.out.println(Arrays.toString(iterateStream.limit(10).toArray()));
 
         Stream<Long> fibonacciSequenceStream = Stream.iterate(new Long[]{0L, 1L}, p -> new Long[]{p[1], p[0] + p[1]}).map(p -> p[0]);
-        System.out.println(Arrays.toString(fibonacciSequenceStream.limit(10).toArray()));
+//        System.out.println(Arrays.toString(fibonacciSequenceStream.limit(10).toArray()));
+
+        //Aula 07
+
+        Stream<Integer> st1 = list.stream()
+                .map(x -> x * 10);
+        st1.forEach(System.out::println);
+
+        int sum = list.stream()
+                .reduce(0, Integer::sum);
+        System.out.println("Sum: " + sum);
+
+        List<Integer> st2 = list.stream()
+                .filter(x -> x % 2 == 0)
+                .map(x -> x * 10).toList();
+        st2.forEach(System.out::println);
+
 
     }
 }
